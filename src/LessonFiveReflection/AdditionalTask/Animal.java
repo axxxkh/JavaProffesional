83aeff1d876a001ef3d06e0f8b0e9355b433b757
O.KH <alxxxkh@gmail.com>
2021-12-15 22:22:25 +0200

    homeworkFive

--- src/LessonFiveReflection/AdditionalTask/Animal.java
+++ src/LessonFiveReflection/AdditionalTask/Animal.java
@@ -1,2 +1,18 @@
-package LessonFiveReflection.AdditionalTask;public class AnimalCat {
+package LessonFiveReflection.AdditionalTask;
+
+import java.time.LocalDateTime;
+
+public class Animal {
+    private final String name = "asd";
+    protected LocalDateTime birthTime = LocalDateTime.now();
+    public String sex = "male";
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", birthTime=" + birthTime +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
 }
